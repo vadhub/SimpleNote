2aeb58db439dbfcedd941476ee8be35538eeb964
vadim <gabderahmanov99@gmail.com>
2023-10-28 15:03:04 +0300

    EditNoteFragment init

---
+++ app/src/main/java/com/abg/simplenote/EditNoteFragment.kt
@@ -0,0 +1,22 @@
+package com.abg.simplenote
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abg.simplenote.databinding.FragmentEditNoteBinding
+
+class EditNoteFragment : Fragment() {
+
+    private lateinit var fragmentEditNoteBinding: FragmentEditNoteBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        fragmentEditNoteBinding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return fragmentEditNoteBinding.root
+    }
+
+}
