9e3cce972522852d4c0fc94b92478e8272412063
vadim <gabderahmanov99@gmail.com>
2023-10-31 13:20:56 +0300

    get list

--- app/src/main/java/com/abg/simplenote/ListNotes.kt
+++ app/src/main/java/com/abg/simplenote/ListNotes.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.abg.simplenote.databinding.FragmentListNotestBinding
 
 class ListNotes : Fragment() {
+
+    private lateinit var binding: FragmentListNotestBinding
+    private val viewModel: NoteViewModel by activityViewModels { NoteViewModelFactory(((requireActivity() as MainActivity).application as App).database.getNoteDao()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_notest, container, false)
+    ): View {
+        binding = FragmentListNotestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = binding.recyclerView
+
+        viewModel.getAll()
+
+        viewModel.noteLiveData.observe(viewLifecycleOwner) {
+            val adapter = NoteAdapter(it)
+            recyclerView.adapter = adapter
+        }
+
     }
 
 }
